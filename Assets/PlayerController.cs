304ad00f48c21e06c9aa457a7ccc9d022a073a1d
Bilal Asry <bilal.asry@elev.ga.ntig.se>
2024-11-12 08:49:38 +0100

    sfyjdfyuk

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   [SerializeField]
+   float jumpForce = 1000;
+
+   bool mayJump = true;
+
+   [SerializeField]
+   Transform groundChecker;
+
+   [SerializeField]
+   LayerMask groundLayer;
+
+
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Jump") > 0 && mayJump == true) 
+        {
+            
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            
+            rb.AddForce(Vector2.up * jumpForce);
+            mayJump = false;
+
+        }
+
+        if(Input.GetAxisRaw("Jump") == 0)
+        {
+            mayJump = true;
+        }
+
+        print(isGrounded());
+
+    }
+
+    private bool isGrounded()
+    {
+     if(Physics2D.OverlapCircle(groundChecker.position, -2f, groundLayer))
+     {
+      return true;
+     }
+     else{
+        return false;
+     }
+    
+    
+    }
+
+}
+    
